5e73a841302ed30379b130d3bcdd0366fd1bf4fd
rams99 <aroldrams@gmail.com>
2020-12-18 20:13:44 +0000

    cors error resolve

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const port = process.env.port || 4545;
 const app = express();
 const router = require('./route/route');
-
+const frontend = 'http://localhost:4200'
+app.use(express.json());
+app.use(cors({origin: frontend}));
 app.use(express.json());
 app.use('/api', router);
 
