bb9ec4443e8422da44efa363c6645362b12747b1
rams99 <aroldrams@gmail.com>
2021-01-04 15:49:22 +0000

    last upadte

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.port || 4545;
 var cors = require('cors');
 const app = express();
 const router = require('./route/route');
-const frontend = 'https://miam-bf.netlify.app/'
+const frontend = 'https://miam-bf.netlify.app'
 app.use(express.json());
 app.use(cors({origin: frontend}));
 app.use(express.json());
